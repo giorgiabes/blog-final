28771890e21617f84c826f76d441bb15e09b8723
giorgiabes <abesadze.george@gmail.com>
2025-03-14 16:59:44 +0400

    add signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,28 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .models import Profile
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = Profile
+    template_name = "profile.html"
+
+    def get_object(self):
+        return self.request.user.profile
+    
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    fields = ["bio", "location", "website"]
+    template_name = "profile_edit.html"
+    success_url = reverse_lazy("profile")
+    
+    def get_object(self):
+        return self.request.user.profile
